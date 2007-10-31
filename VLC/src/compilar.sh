55aa93a754cc62e4a0d7af71b4364e06ee587764
Nestor Morales <nestor@isaatc.ull.es>
2007-10-31 12:45:26 +0000

    Proyecto VLC con modificaciones para leer de múltiples cámaras y creación de memoria compartida

---
+++ VLC/src/compilar.sh
@@ -0,0 +1,5 @@
+rm libdshow_plugin.dll
+make
+cp libdshow_plugin.dll C:/Proyecto/Leeimagen/dll/plugins
+cp libdshow_plugin.dll "C:/Archivos de Programa/VideoLAN/VLC/plugins"
+cp libdshow_plugin.dll "C:/Proyecto/PruebasGPS/GPSconImagenes/dll/plugins"
