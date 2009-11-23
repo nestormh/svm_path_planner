a3d90e9deee7c9467ace41e8d114acda2cb2ec74
Alberto Hamilton <alberto@isaatc.ull.es>
2009-11-23 09:18:38 +0000

    Boton para salver posicion actual en fichero en directorio Sitios

--- Jade/src/sibtra/gps/PanelGPSTriumph.java
+++ Jade/src/sibtra/gps/PanelGPSTriumph.java
@@ -1,5 +1,14 @@
 package sibtra.gps;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import sibtra.util.LabelDatoFormato;
@@ -21,6 +30,10 @@ public class PanelGPSTriumph extends PanelMuestraGPSData {
 	private LabelDatoFormato ldCal;
 
 	private LabelDatoFormato ldOK;
+	
+	private AbstractAction accionSalvaPutoActual;
+	
+	private JFileChooser fc;
 
 	public PanelGPSTriumph(GPSConnectionTriumph gct) {
 		super(true); //solo espaciales
@@ -34,8 +47,50 @@ public class PanelGPSTriumph extends PanelMuestraGPSData {
 		ldOK=new LabelDatoFormato(GPSConnectionTriumph.class,"getNumOKLink"," %10d");
 //		lda.setPreferredSize(new Dimension(100,50));
 		añadeAPanel(ldOK, "Paq OK Enlace");
+		//Botón salvar posición
+		accionSalvaPutoActual=new AccionSalvaPosicion();
+		JButton jbSalvaPos=new JButton(accionSalvaPutoActual);
+		añadeAPanel(jbSalvaPos,"Grabar posición");
+		
+        //elegir fichero
+        fc = new JFileChooser(new File("./Sitios"));
+		
 	}
 
+	/**
+	 * @return the accionSalvaPutoActual
+	 */
+	public AbstractAction getAccionSalvaPutoActual() {
+		return accionSalvaPutoActual;
+	}
+
+	class AccionSalvaPosicion extends AbstractAction {
+
+		public AccionSalvaPosicion() {
+			super("Grabar Posición");
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			int devuelto = fc.showSaveDialog(PanelGPSTriumph.this);
+			if (devuelto == JFileChooser.APPROVE_OPTION) {
+				try {
+					File file = fc.getSelectedFile();
+					GPSData ptoAct=gpsCT.getPuntoActualTemporal();
+					//                gpsCon.saveRuta(file.getAbsolutePath());
+					ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+					oos.writeObject(ptoAct);
+					oos.close();
+				} catch (IOException ioe) {
+					System.err.println("Error al escribir en el fichero con posición actual");
+					System.err.println(ioe.getMessage());
+				}
+
+			}
+		}
+		
+	}
+	
 	/** Actualiza en base al ultimo punto temporal */
 	protected void actualiza() {
 		//TODO actualizacion ldCAL etc.
