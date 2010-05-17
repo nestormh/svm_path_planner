82600bfe54ac92bff068400d2327b9d587581a09
Alberto Hamilton <alberto@isaatc.ull.es>
2010-05-17 10:27:14 +0000

    Probamos con modelos a ver si entendemos.

--- gazebo/position.cc
+++ gazebo/position.cc
@@ -1,6 +1,41 @@
 #include <gazebo/gazebo.h>
 #include <gazebo/GazeboError.hh>
 
+/*
+std::string pose2string(gazebo::Pose ps){
+	std::stringstream s;
+	s<<"("<<ps.pos.x<<","<<ps.pos.y<<","<<ps.pos.z<<")";
+	s<<" ["<<ps.roll<<"|"<<ps.pitch<<"|"<<ps.yaw<<"]";
+	return s.str();
+}
+
+void muestraData(gazebo::PositionData* data) {
+	std::cout<< "enableMot:" << data->cmdEnableMotors
+	<<" cmdVel:" << pose2string(data->cmdVelocity)
+	<<" pose:"<< pose2string(data->pose)
+	<<" stall:"<< data->stall
+	<<" velo:"<< pose2string(data->velocity)
+	<<"\n";
+	
+}
+*/
+
+std::string pose2string(gazebo::Pose ps){
+	std::stringstream s;
+	s<<" "<<ps.pos.x<<" "<<ps.pos.y<<" "<<ps.pos.z<<"  ";
+	s<<" "<<ps.roll<<" "<<ps.pitch<<" "<<ps.yaw<<"  ";
+	return s.str();
+}
+
+void muestraData(gazebo::PositionData* data) {
+	std::cout
+	<< pose2string(data->cmdVelocity)
+	<< pose2string(data->pose)
+	<< pose2string(data->velocity)
+	<<"\n";
+	
+}
+
 int main()
 {
 	gazebo::Client *client = new gazebo::Client();
@@ -37,8 +72,9 @@ int main()
 	std::cout << "Open the Position interface\n" ;
 	try
 	{
-	posIface->Open(client, "simpleCar_model::position_iface_0");
+//	posIface->Open(client, "simpleCar_model::position_iface_0");
 //		 posIface->Open(client, "pioneer2dx_model1::position_iface_0");
+		 posIface->Open(client, "lince_model::position_iface_0");
 	}
 	catch (std::string e)
 	{
@@ -52,27 +88,33 @@ int main()
 
 	posIface->Lock(1);
 	posIface->data->cmdEnableMotors = 1;
+	muestraData(posIface->data);
 	posIface->Unlock();
 
 	int i=0;
-	while (i<10)
+	while (i<200)
 	{
 		
 		posIface->Lock(1);
-// posIface->data->cmdVelocity.yaw = -0.1;
-		if(i%2) {
-			double vel=0.2*(i+1);
-			posIface->data->cmdVelocity.pos.x = vel;
-			std::cout << "Velocidad x="<< vel<<"\n";
-		} else {
-			std::cout << "Velocidad x=0\n";
-			posIface->data->cmdVelocity.pos.x = 0;
+ posIface->data->cmdVelocity.yaw = -0.1;
+		if(!(i%10)) {
+			if((i/10)%2) {
+				double vel=0.2*(i/10+1);
+				posIface->data->cmdVelocity.pos.x = vel;
+				//std::cout << "Velocidad x="<< vel<<"\n";
+			} else {
+				//std::cout << "Velocidad x=0\n";
+				posIface->data->cmdVelocity.pos.x = 0;
+			}
 		}
+
+		muestraData(posIface->data);
 		posIface->Unlock();
 		
-		sleep(2);
-		//		 usleep(100000);
+		//sleep(2);
+		usleep(100000);
 		i++;
 	}
 	return 0;
 }
+
